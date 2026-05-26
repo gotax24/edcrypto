1289f12e4e235cc9b2e255f5d0c39a6c7bdd5008
Ernesto Bracho R. <dev.ejbr@gmail.com>
2026-05-25 22:49:20 -0400

    feat: agregar soporte para React Query y mejorar la estructura del proveedor en la aplicación

--- src/App.tsx
+++ src/App.tsx
@@ -4,21 +4,23 @@ import CoinsContainer from "./components/CoinsContainer";
 import NotFound from "./components/NotFound";
 import WatchlistContainer from "./components/WatchlistContainer";
 import CoinContainer from "./components/CoinContainer";
+import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
 //import FavoriteProvider from "./context/FavoriteProvider";
 
 const App = () => {
   return (
     //<FavoriteProvider>
-      <BrowserRouter>
-        <Routes>
-          <Route path="/" element={<Layout />}>
-            <Route index element={<CoinsContainer />} />
-            <Route path="watchlist" element={<WatchlistContainer />} />
-            <Route path="coin/:id" element={<CoinContainer />} />
-            <Route path="*" element={<NotFound />} />
-          </Route>
-        </Routes>
-      </BrowserRouter>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Layout />}>
+          <Route index element={<CoinsContainer />} />
+          <Route path="watchlist" element={<WatchlistContainer />} />
+          <Route path="coin/:id" element={<CoinContainer />} />
+          <Route path="*" element={<NotFound />} />
+        </Route>
+      </Routes>
+      <ReactQueryDevtools initialIsOpen={false} />
+    </BrowserRouter>
     //</FavoriteProvider>
   );
 };
